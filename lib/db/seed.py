4cd951b4465acb63a674ac6f6080edf6b1c79a2f
Sarah Jones <sarahrjones8@gmail.com>
2023-05-15 22:06:23 -0400

    Worked to fill data for all 3 tables. Issue arose with the genre's table, because it is missing a column

---
+++ lib/db/seed.py
@@ -0,0 +1,110 @@
+from faker import Faker
+import random
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+from models import Fan, Review, Genre
+
+
+if __name__ == '__main__':
+   engine = create_engine('sqlite:///genre_app.db')
+   Session = sessionmaker(bind=engine)
+   session = Session()
+
+
+   session.query(Fan).delete()
+   session.query(Genre).delete()
+   session.query(Review).delete()
+
+
+   fake = Faker()
+
+
+   # Genre Table
+   genres = []
+   showtunes = Genre(name="Showtunes", avg_bpm=117, style="Instrumental", prodominent_instrument="Piano", commonly_known="Yes")
+   glam_rock = Genre(name="Glam Rock ", avg_bpm=125, style="Instrumental", prodominent_instrument="Piano", commonly_known="No")
+   k_pop = Genre(name="K-pop", avg_bpm=175, style="Instrumental", prodominent_instrument="Guitar", commonly_known="Yes")
+   punk_jazz = Genre(name="Punk Jazz", avg_bpm=125, style="Instrumental", prodominent_instrument="Guitar", commonly_known="No")
+   swing = Genre(name="Swing", avg_bpm=132, style="Instrumental", prodominent_instrument="Drums", commonly_known="Yes")
+   ska = Genre(name="Ska", avg_bpm=216, style="Instrumental", prodominent_instrument="Drums", commonly_known="No")
+   jungle = Genre(name="Jungle", avg_bpm=160, style="Electronic", prodominent_instrument="Piano", commonly_known="Yes")
+   psychedelic_trance = Genre(name="Psychedelic Trance", avg_bpm=140, style="Electronic", prodominent_instrument="Piano", commonly_known="No")
+   disco = Genre(name="Disco", avg_bpm=120, style="Electronic", prodominent_instrument="Guitar", commonly_known="Yes")
+   acid_breaks = Genre(name="Acid Breaks", avg_bpm=135, style="Electronic", prodominent_instrument="Guitar", commonly_known="No")
+   house = Genre(name="House", avg_bpm=135, style="Electronic", prodominent_instrument="Drums", commonly_known="Yes")
+   uplifting_trance = Genre(name="Uplifting Trace", avg_bpm=136, style="Electronic", prodominent_instrument="Drums", commonly_known="No")
+   r_and_b = Genre(name="R&B", avg_bpm=70, style="Instrumental", prodominent_instrument="Piano", commonly_known="Yes")
+   waltz = Genre(name="Waltz", avg_bpm=87, style="Instrumental", prodominent_instrument="Piano", commonly_known="No")
+   reggae = Genre(name="Reggae", avg_bpm=75, style="Instrumental", prodominent_instrument="Guitar", commonly_known="Yes")
+   bluegrass = Genre(name="Bluegrass", avg_bpm=95, style="Instrumental", prodominent_instrument="Guitar", commonly_known="No")
+   blues = Genre(name="Blues", avg_bpm=70, style="Instrumental", prodominent_instrument="Drums", commonly_known="Yes")
+   smooth_jazz = Genre(name="Smooth Jazz", avg_bpm=101, style="Instrumental", prodominent_instrument="Drums", commonly_known="No")
+   hip_hop = Genre(name="Hip Hop", avg_bpm=90, style="Electronic", prodominent_instrument="Piano", commonly_known="Yes")
+   ambient_music = Genre(name="Ambient Music", avg_bpm=65, style="Electronic", prodominent_instrument="Piano", commonly_known="No")
+   lofi_hip_hop = Genre(name="Lofi Hip Hop", avg_bpm=75, style="Electronic", prodominent_instrument="Guitar", commonly_known="Yes")
+   glitch_hop = Genre(name="Glitch-hop", avg_bpm=100, style="Electronic", prodominent_instrument="Guitar", commonly_known="No")
+   dubstep = Genre(name="Dubstep", avg_bpm=85, style="Electronic", prodominent_instrument="Drums", commonly_known="Yes")
+   downtempo = Genre(name="Downtempo", avg_bpm=85, style="Electronic", prodominent_instrument="Drums", commonly_known="No")
+
+   session.add(showtunes)
+   session.add(glam_rock)
+   session.add(k_pop)
+   session.add(punk_jazz)
+   session.add(swing)
+   session.add(ska)
+   session.add(jungle)
+   session.add(psychedelic_trance)
+   session.add(disco)
+   session.add(acid_breaks)
+   session.add(house)
+   session.add(uplifting_trance)
+   session.add(r_and_b)
+   session.add(waltz)
+   session.add(reggae)
+   session.add(bluegrass)
+   session.add(blues)
+   session.add(smooth_jazz)
+   session.add(hip_hop)
+   session.add(ambient_music)
+   session.add(lofi_hip_hop)
+   session.add(glitch_hop)
+   session.add(dubstep)
+   session.add(downtempo)
+
+   session.commit()
+
+   genres.append(showtunes, glam_rock, k_pop, punk_jazz, swing, ska, jungle, psychedelic_trance, disco, acid_breaks, house, uplifting_trance, r_and_b, waltz, reggae, bluegrass, blues, smooth_jazz, hip_hop, ambient_music, lofi_hip_hop, glitch_hop, dubstep, downtempo)
+
+
+   # Fan Table
+   fans = []
+   for i in range(12):
+       fan = Fan(
+           first_name=fake.first_name(),
+           last_name=fake.last_name()
+       )
+       session.add(fan)
+       session.commit()
+       fans.append(fan)
+  
+   # Review Table
+   reviews = []
+   for fan in fans:
+       for i in range(12):
+        # create reviews that are associated with fans AND ALSO genres
+        review = Review(
+           star_rating=random.randint(1, 5),
+           fan_id=fan.id,
+           genre_id=random.choice(genres).id
+        )
+        reviews.append(review)
+        session.bulk_save_objects(reviews)
+           
+
+
+
+   session.commit()
+   session.close()
